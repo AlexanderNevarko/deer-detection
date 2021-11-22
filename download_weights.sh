829410e6535a533075096b2051b12deaab60ecf8
Ilya Basharov <ilya.basharov.98@mail.ru>
2021-11-22 18:22:17 +0300

    Update download_weights.sh

--- download_weights.sh
+++ download_weights.sh
@@ -6,9 +6,9 @@ OUTPUT_PATH=/app/work_dir/pretrained
 mkdir -p ${OUTPUT_PATH}
 
 #for detection
-DETECTION_NET='https://drive.google.com/file/d/1-G4JUK4adVH7OpfGw6ycxZn5cCDAna_5/view?usp=sharing'
+DETECTION_NET='1-G4JUK4adVH7OpfGw6ycxZn5cCDAna_5'
 gdown --id ${DETECTION_NET} --fuzzy --output ${OUTPUT_PATH}/detection.pth
 
 #for colors
-CLASSIFICATION_NET='https://drive.google.com/file/d/1-jK92IIoPk8YKqYdYEqFhyePRrV8-GBO/view?usp=sharing'
+CLASSIFICATION_NET='1-jK92IIoPk8YKqYdYEqFhyePRrV8-GBO'
 gdown --id ${COLOR_NET} --fuzzy --output ${OUTPUT_PATH}/classification.pth
